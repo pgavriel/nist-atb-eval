8690a610e227cadfb6d77fdeeccc3b54c9748ec6
pgavriel <pgavriel@mail.middlesex.edu>
2021-01-26 15:56:23 -0500

    img split update

--- src/roi_img_split.py
+++ src/roi_img_split.py
@@ -14,22 +14,28 @@ def naming_scheme(filename):
     return filename[:3]
 
 if __name__ == '__main__':
-    source_path = '/home/pgavriel/ros_ws/src/ros_picam/captures'
-    save_path = '/home/pgavriel/Pictures/tb_data'
-    roi_csv = '/home/pgavriel/ros_ws/src/nist_atb_eval/config/tb_roi.csv'
+    source_path = '/home/csrobot/taskboard_ws/src/nist-atb-eval/data/full'
+    save_path = '/home/csrobot/tb_data'
+    roi_csv = '/home/csrobot/taskboard_ws/src/nist-atb-eval/config/tb_roi.csv'
 
     file_list = [f for f in listdir(source_path) if isfile(join(source_path, f))]
+    print("FILE LIST:")
     print(file_list)
 
     # Create and move to output/save directory
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     os.chdir(save_path)
+    print("MOVED TO SAVE DIR: {}".format(save_path))
 
     # Load ROI from .csv
     img = cv2.imread(join(source_path,file_list[0]))
+    print("ROI LOADED:")
     roi = getROI(roi_csv,img.shape[0],img.shape[1])
-    print(roi)
+
+    #scores = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+    scores = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+    print(scores)
 
     size = 50
     for f in file_list:
@@ -45,12 +51,12 @@ if __name__ == '__main__':
                 crop = img[y-s:y+s, x-s:x+s]
                 crop = cv2.resize(crop, (96,96), interpolation = cv2.INTER_AREA)
                 image = Image.fromarray(crop)
-                folder = "{:02d}".format(counter)
+                folder = "{:02d}-{}".format(counter,r[0])
                 if not os.path.exists(folder):
                     os.makedirs(folder)
                 _, _, files = next(os.walk(folder))
                 file_count = "{:04d}".format(len(files)+1)
-                filename = "{}/{}-{}.png".format(folder,naming_scheme(f),file_count)
+                filename = "{}/{}-{}.png".format(folder,scores[counter-1],file_count)
                 print("Saving {}".format(filename))
                 image.save(filename)
                 counter = counter + 1
